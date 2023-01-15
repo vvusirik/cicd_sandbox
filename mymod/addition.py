6b1d4e182ec651cc9ecb428aeb306ad61cbcddb1
Vishal Vusirikala <vvusirik@gmail.com>
2023-01-15 12:56:33 -0500

    test add3

--- mymod/addition.py
+++ mymod/addition.py
@@ -1,2 +1,6 @@
 def add(x, y):
-    return x +y 
+    return x + y
+
+
+def add3(x, y, z):
+    return x + y + z
--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,4 +1,7 @@
-from mymod.addition import add
+from mymod.addition import add, add3
 
 def test_add():
     assert add(1, 2) == 3
+
+def test_add3():
+    assert add3(1, 2, 3) == 6
