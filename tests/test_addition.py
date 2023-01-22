be89223f640aaa8f8161cc16db13057b64b4980c
Vishal Vusirikala <vvusirik@gmail.com>
2023-01-22 12:37:20 -0500

    pre-commit

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,7 +1,9 @@
 from mymod.addition import add, add3
 
+
 def test_add():
     assert add(1, 2) == 3
+
 
 def test_add3():
     assert add3(1, 2, 3) == 6
--- tests/test_subtraction.py
+++ tests/test_subtraction.py
@@ -1,4 +1,5 @@
 from mymod.subtraction import subtract
 
+
 def test_subtract():
     assert subtract(2, 1) == 1
